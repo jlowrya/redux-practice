5372b5a5d05b0c3d01ee20af5be6e9e2521ea1ed
James Lowry <jl@balkerne.com>
2019-09-08 20:01:18 +0100

    Created reducer function with ADD_TODO action functionality

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+function todos(state = [], action){
+  if(action.type === 'ADD_TODO'){
+    return state.concat([action.todo])
+  }
+
+  return state
+}
+
+
 function createStore(){
   let state
   let listeners = []
