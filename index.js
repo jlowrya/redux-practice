d47eeb3428762f00e2dead13b81c84cec7a31321
James Lowry <jl@balkerne.com>
2019-09-08 19:18:39 +0100

    Added subscribe function that returns a function that can be used to unsubscribe

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 function createStore(){
   let state
+  let listeners = []
 
   const getState = () => state
+
+  const subscribe = (listener) => {
+    listeners.push(listener)
+    return () => {
+      listeners = listeners.filter((l) => l !== listener)
+    }
+  }
 
   return {
     getState
   }
 }
+
+const store = createStore()
+store.subscribe(() => {
+  console.log('The new state is: ', store.getState())
+})
+
+store.subscribe(() => {
+  console.log('The store changed.')
+})
