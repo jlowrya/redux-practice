809ea680e9b26c166062a615588bba2d7be0b7ed
James Lowry <jl@balkerne.com>
2019-09-08 19:19:03 +0100

    Removed test code

--- index.js
+++ index.js
@@ -15,12 +15,3 @@ function createStore(){
     getState
   }
 }
-
-const store = createStore()
-store.subscribe(() => {
-  console.log('The new state is: ', store.getState())
-})
-
-store.subscribe(() => {
-  console.log('The store changed.')
-})
