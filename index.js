26afaac73878acf993917c16351cda22238657c7
James Lowry <jl@balkerne.com>
2019-09-08 23:10:02 +0100

    Created general reducer function to combine goals and todo reducers

--- index.js
+++ index.js
@@ -40,7 +40,25 @@ function todos(state = [], action){
   }
 }
 
-const store = createStore(todos)
+function goals(state = [], action) {
+  switch(action.type){
+    case 'ADD_GOAL':
+      return state.concat([action.goal])
+    case 'REMOVE_GOAL':
+      return state.filter((goal) => goal.id !== action.id)
+    default:
+      return state
+  }
+}
+
+function app(state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action),
+  }
+}
+
+const store = createStore(app)
 
 store.subscribe(() => {
   console.log('The new state is:  ', store.getState())
